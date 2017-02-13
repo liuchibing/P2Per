f32d1492e46a6dfc8b606088a34edf152752e5fa
liuchibing <charlie428b@hotmail.com>
2017-02-13 16:59:42 +0800

    Initialize

---
+++ P2PerClient/src/ml/liuchibing/P2PerClient/Main.java
@@ -0,0 +1,7 @@
+package ml.liuchibing.P2PerClient;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ P2PerServer/src/ml/liuchibing/P2PerServer/Main.java
@@ -0,0 +1,9 @@
+package ml.liuchibing.P2PerServer;
+
+import java.net.*;
+
+public class Main {
+    public static void main(String[] args) {
+        DatagramSocket socket = new DatagramSocket()
+    }
+}
